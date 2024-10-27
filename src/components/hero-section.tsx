b1eb41521243f14af9ec4663a24b794b5c500a3b
Alejandro Fernandez <fernandez.afb@gmail.com>
2024-10-27 14:50:40 +0700

    Remove unused imports and files

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ArrowRight, Instagram, Twitter, Facebook, Linkedin, Globe } from "lucide-react"
+import { ArrowRight, Instagram, Linkedin, Globe } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
 import {
